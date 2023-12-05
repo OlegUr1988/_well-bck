5eb9de36332eaa176011c61c350f1c94ccf5ecfb
Doniyor Alimov <alimov.doniyor28@gmail.com>
2023-12-05 13:41:27 +0500

    Adding static files

--- index.ts
+++ index.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import assets from "./routes/assets";
 import cors from "./middlewares/cors";
+import path from "path";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors);
+app.use(express.static("dist"));
 app.use("/api/assets", assets);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listen on http:\\\\localhost:${port}`));
